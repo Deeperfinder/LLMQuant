ebff1c8b0913b9fc740e2eed6850cf894eb2c494
xf <867377995@qq.com>
2025-10-20 14:30:43 +0800

    [add] add class AWQLinear_GEMM apply linear

---
+++ quant/nn_models/modules/linear/linear_base.py
@@ -0,0 +1,14 @@
+import torch
+
+class LinearBase(torch.nn.Module):
+    """
+        Base linear layer.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def forward(
+        self,x
+    ):
+        raise NotImplementedError
+        
